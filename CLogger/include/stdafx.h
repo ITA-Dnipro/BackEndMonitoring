0a4074dcd72fce72829c66bafed9f2e48a1098b5
Сафиюлин Александр <rootalfa4@gmail.com>
2021-01-08 13:45:37 +0200

    DllMain and precompiled header added

---
+++ CLogger/include/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DEMODLL_EXPORTS
+
+//#define CLOGGER_EXTERN_C extern "C"
+
+#ifdef DEMODLL_EXPORTS
+	#define CLOGGER_API __declspec(dllexport)
+#else
+	#define CLOGGER_API __declspec(dllimport)
+#endif
+
+#include <string>
