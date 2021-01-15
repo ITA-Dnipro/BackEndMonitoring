4985e33a746cf9c31f85d3e8aae19de98483b1a3
Сафиюлин Александр <rootalfa4@gmail.com>
2021-01-15 14:53:39 +0200

    Disabled warnings 4251, 5105

--- CLogger/include/CLogMessage/CLogMessage.h
+++ CLogger/include/CLogMessage/CLogMessage.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma warning( disable : 4251 )
 #include "stdafx.h"
 
 #include "ELogLevel/ELogLevel.h"
--- CLogger/include/stdafx.h
+++ CLogger/include/stdafx.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma warning(disable : 4251)
 
 #define DEMODLL_EXPORTS
 
