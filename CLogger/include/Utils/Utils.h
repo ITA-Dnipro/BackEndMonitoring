109aa50e4d3ff3bba6fc5358a829a76434f3df2a
Chupakabra0 <rootalfa4@gmail.com>
2021-01-20 19:17:53 +0200

    Update: more simple and universal time get function

--- CLogger/include/Utils/Utils.h
+++ CLogger/include/Utils/Utils.h
@@ -7,32 +7,6 @@
 namespace LogUtils
 {
 	/// <summary>
-	///		Casts <c>int</c> to <c>std::string</c> with adding
-	///		a zero at the start of the string if its need
-	/// </summary>
-	/// <param name="value">
-	///		Value to cast to integer
-	/// </param>
-	/// <returns>
-	///		String, that contains two-digit number
-	/// </returns>
-	/// <example>
-	///		auto number = 123;
-	///		std::string twoDigit = AddZero(number);
-	/// </example>
-	inline std::string AddZero(const int value)
-	{
-		auto str = std::to_string(value);
-
-		if (value < 10)
-		{
-			str.insert(str.begin(), '0');
-		}
-
-		return str;
-	}
-
-	/// <summary>
 	///		Gets current system time in <c>std::string</c>
 	/// </summary>
 	/// <returns>
@@ -43,22 +17,17 @@ namespace LogUtils
 	/// </example>
 	inline std::string GetCurrentTimeString()
 	{
-		constexpr auto special_year = 1900;
 		const auto time = std::make_unique<tm>();
 		auto current_time = std::chrono::system_clock::to_time_t
 		(std::chrono::system_clock::now());
 
 		localtime_s(time.get(), &current_time);
 
-		const auto day   = AddZero(time->tm_mday);
-		const auto month = AddZero(time->tm_mon + 1);
-		const auto year  = AddZero(time->tm_year + special_year);
-		const auto hour  = AddZero(time->tm_hour);
-		const auto min   = AddZero(time->tm_min);
-		const auto sec   = AddZero(time->tm_sec);
+		std::stringstream ss;
+		ss.imbue(std::locale());
+		ss << std::put_time(time.get(), "%c");
 
-		return day + "." + month + "." + year + " "
-			+ hour + ":" + min + ":" + sec;
+		return ss.str();
 	}
 	
 	/// <summary>
