9423f644c7765c4baef473c7155635b768d5573f
KokhanO <72404793+KokhanO@users.noreply.github.com>
2021-02-02 13:05:02 +0200

    Xml util (#53)
    * Added pugixml lib
    
    * Added XMLParser
    
    * Fixed all bugs
    
    * Added strategy pattern in CConfiguration class
    
    added common interface for parsers,
    added Configuration class with strategy pattern usage,
    Fixed small logic and code style issues.
    
    example of Configuration usage:
    std::unique_ptr<IParser> p = std::make_unique<CXMLParser>();
        p->Initialize("D:\\xgconsole.xml");
        CConfiguration c(std::move(p));
        std::string ipaddress;
        if (c.TryToGetConfiguration("//root/Server/ipaddress", ipaddress))
        {
            std::cout << ipaddress << std::endl;
        }
    
    * Added some verifications of transfered parameters
    
    added verificaions in XMLParser, whether pointer to file exists, if Parcer wasn't able to find data with given data path and if data is equal to enable of disable
    
    * Changed all architecture
    
    the only solution that has come in a long time. Reworked all logic. Now returning structures with needed data. A lot of hardcode is present. If logic is ok, next task is to logg errors and exceptions. Also pugixml returns 38 warnings!
    
    * Added one read method which reads all configurations, fixed bugs
    
    std::unique_ptr<IParser> p = std::move(std::make_unique<CXMLParser>());
        p->Initialize("D:\\xgconsole.xml");
        CConfiguration config(std::move(p));
        config.ReadConfigFromFile();
    
    example of Configuration class usage.
    All possible nullpointers exception prevented, I do believe.
    
    * Data Reader
    
    * Added Settings classes
    
    * A little start for great things...
    
    * small fixes
    
    * consts and explicits
    
    * Added filters for Settings classes
    
    * Code conventions
    
    * Added interfaces
    
    * Added time in hdd and processes
    
    * fixed mistakes
    
    * resolved conflicts
    
    Co-authored-by: Chupakabra0 <rootalfa4@gmail.com>

--- utils/include/CLoggingSettings.h
+++ utils/include/CLoggingSettings.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "CDataReader.h"
 #include "CLogger/include/Log.h"
+#include "CSettings.h"
 
-class CLoggingSettings
+class CLoggingSettings : public CSettings
 {
 public:
 	CLoggingSettings() = delete;
@@ -10,16 +11,15 @@ public:
 	CLoggingSettings(CLoggingSettings&&) = delete;
 	
 	explicit CLoggingSettings(std::shared_ptr<CDataReader> p_data_reader);
-	~CLoggingSettings() = default;
+	~CLoggingSettings() override = default;
 
-	void ReadConfigurationFromFile();
+	void ReadConfigurationFromFile() override;
 
 	[[nodiscard]] ELogLevel GetLogLevel() const;
 	[[nodiscard]] ELogFlush GetFlush() const;
 	[[nodiscard]] std::string GetFileName() const;
 
 private:
-	std::shared_ptr<CDataReader> m_p_data_reader_;
 	ELogLevel m_log_level_;
 	ELogFlush m_log_flush_;
 	std::string m_file_name_;
