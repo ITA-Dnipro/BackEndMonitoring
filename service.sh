27dd4892c4a3ecd97ee4a1eabe74703e0dfc16af
akolechko <kolechkoandrey@gmail.com>
2021-02-03 17:10:54 +0200

    Update shell script

--- service.sh
+++ service.sh
@@ -12,6 +12,44 @@ DOT_SERVICE_SYMLINK=/etc/systemd/system/$DOT_SERVICE_NAME
 BINARY_DEST=$MAIN_FOLDER$BINARY_NAME
 XML_CONFIG_DEST=$MAIN_FOLDER$XML_CONFIG_NAME
 
+main()
+{
+    check_sudo
+    handle_cmd $1
+}
+
+handle_cmd()
+{
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "" ]
+    then
+        show_help
+
+    elif [ "$1" = "-i" ] || [ "$1" = "--install" ]
+    then
+        install
+
+    elif [ "$1" = "-u" ] || [ "$1" = "--uninstall" ]
+    then
+        uninstall
+
+    elif [ "$1" = "--start" ]
+    then
+        start
+
+    elif [ "$1" = "--stop" ]
+    then
+        stop
+
+    elif [ "$1" = "--status" ]
+    then
+        status
+
+    else 
+        echo "Invalid flag"
+        
+    fi
+}
+
 install()
 {
     PARENT_PATH=$( cd "$(dirname "${BASH_ORIG[0]}")" ; pwd -P )
@@ -25,16 +63,21 @@ install()
     ln -s $DOT_SERVICE_ORIG $DOT_SERVICE_SYMLINK
 
     systemctl enable $DOT_SERVICE_NAME
-    systemctl start $DOT_SERVICE_NAME
+    start
 }
 
 uninstall()
 {
-    systemctl stop $DOT_SERVICE_NAME
+    stop
     systemctl disable $DOT_SERVICE_NAME
 
     rm $DOT_SERVICE_ORIG
     rm -r $MAIN_FOLDER
+}
+
+status()
+{
+    systemctl status $DOT_SERVICE_NAME
 }
 
 start()
@@ -47,28 +90,24 @@ stop()
     systemctl stop $DOT_SERVICE_NAME
 }
 
-handle_cmd()
+check_sudo()
 {
-    if [ $1 = "-install" ]
+    if [ $(/usr/bin/id -u) -ne 0 ]
     then
-        install
-
-    elif [ $1 = "-uninstall" ]
-    then
-        uninstall
-
-    elif [ $1 = "-start" ]
-    then
-        start
-
-    elif [ $1 = "-stop" ]
-    then
-        stop
-
-    else 
-        echo "Invalid flag"
-        
+        echo "sudo required"
+        exit
     fi
 }
 
-handle_cmd $1
+show_help()
+{
+    echo "Backend monitoring server"
+    echo "Usage:"
+    echo "-i / --install   - install and start the service"
+    echo "-u / --uninstall - stop and uninstall the service"
+    echo "--start          - start the service"
+    echo "--stop           - stop the service"
+    echo "--status         - show current status"
+}
+
+main $1
