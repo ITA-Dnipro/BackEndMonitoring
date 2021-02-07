5e2cd3bdbfd45b5bfe23e8c53bf2619f6b8e528c
Chupakabra0 <rootalfa4@gmail.com>
2021-02-07 17:20:46 +0200

    Update: added folder for global logger

--- CLogger/include/CLogMacro/CLogUsageMacro.h
+++ CLogger/include/CLogMacro/CLogUsageMacro.h
@@ -2,7 +2,7 @@
 #include "stdafx.h"
 
 #include "Utils/Utils.h"
-#include "GlobalLogger.h"
+#include "CLogger/include/CLoggerGlobal/GlobalLogger.h"
 
 // Takes fifteenth argument from parameter pack
 #define TAKE_15(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, N, ...) \
